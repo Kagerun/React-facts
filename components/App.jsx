427c55bae9def89e53697b60a6b49534b78412fc
Kagerun <timur.panayirci@gmail.com>
2026-08-31 17:29:22 +0300

    final

---
+++ components/App.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./Navbar.jsx";
+import { Main } from "./Main.jsx";
+
+export function App(){
+    return (
+        <>
+        <Navbar />
+        <Main />
+        </>
+    )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+export function Navbar(){
+    return (
+        <header className='header'>
+            <nav className= "navbar">
+                <img className='nav-logo' src='../images/react-logo.png' alt='react logo'></img>
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
